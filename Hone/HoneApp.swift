511d1c72b6bc8f5451b750231b08ca602c655fa1
kailey-batman <kailey@fieldguide.io>
2026-05-20 12:32:49 -0400

    Initial Commit

---
+++ Hone/HoneApp.swift
@@ -0,0 +1,17 @@
+//
+//  HoneApp.swift
+//  Hone
+//
+//  Created by Kailey Thorpe on 5/20/26.
+//
+
+import SwiftUI
+
+@main
+struct HoneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
